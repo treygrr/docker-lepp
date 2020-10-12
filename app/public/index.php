1a3e7f717d6f00e5c32569b5420017dd30d4774a
Trey <phlasc@gmail.com>
2020-10-12 11:17:46 -0400

    Update configs

--- app/public/index.php
+++ app/public/index.php
@@ -1,1 +1,8 @@
-<?php phpinfo(); ?>
+<?php 
+$what = 'what';
+echo "help {$what} me";
+
+$db_connection = pg_connect("host=postgres dbname=testdb user=postgres password=password");
+
+print_r($db_connection);
+?>
