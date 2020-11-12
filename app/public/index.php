cbad5aad8a7f5f6a97de398883dd9f439c997412
Trey <phlasc@gmail.com>
2020-11-12 13:26:27 -0500

    Misc

--- app/public/index.php
+++ app/public/index.php
@@ -1,8 +1,3 @@
-<?php 
-$what = 'what';
-echo "help {$what} me";
-
-$db_connection = pg_connect("host=postgres dbname=testdb user=postgres password=password");
-
-print_r($db_connection);
+<?php
+    echo 'hello world';
 ?>
